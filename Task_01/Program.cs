1c4700c448ed14c8435c66fa7d0a83bb9e44ffd3
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-13 17:37:22 +0300

    Task_01 completed

---
+++ Task_01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void fillarray(int[] number)
+{
+    int len = number.Length;
+    int i = 0;
+    while (i < len)
+    {
+        number[i] = new Random().Next(0, 2);
+        i++;
+    }
+}
+
+void printarray(int[] elements)
+{
+    int length = elements.Length;
+    int i = 0;
+    while (i < length)
+    {
+        Console.Write($"{elements[i]} ");
+        i++;
+    }
+}
+
+int[] array = new int[8];
+fillarray(array);
+printarray(array);
